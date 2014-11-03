363e6bd4c3042ae25e416253176718be8f7d09e6
vilion <takabatakekoichi@gmail.com>
2014-11-03 17:02:40 +0900

    一旦戻す

--- TelephoneNumbers.py
+++ TelephoneNumbers.py
@@ -51,9 +51,6 @@ class TreeNode:
             child = TreeNode(v, node)
             node.add_child(child)
             node = child
-
-    def __str__(self):
-        return str(self.get_val())
         
 
 if __name__ == "__main__":
@@ -64,8 +61,8 @@ if __name__ == "__main__":
         "0412199803",
         "0468892011",
         "112",
-        "15",
-        # "0468309302",
+        "156",
+        "0468309302",
     ]
     # for i in xrange(N):
     for i, num in enumerate(telnums):
@@ -79,20 +76,24 @@ if __name__ == "__main__":
         )
         if tree:
             node = tree[0].get_root()
-            children = node.get_children()
             for i, n in enumerate(telephone):
-                print >> sys.stderr, "children: " + str("".join([ child.get_val() for child in children]))
-
-                child_list = filter(lambda c: c.get_val() != n, children )
-                if not child_list:
-                    rest_tel_num = telephone[i:]
-                    node.add_tree(rest_tel_num)
-                    print >> sys.stderr, rest_tel_num
-                    break
-                else:
-                    node = child_list[0]
+                if node.get_val() == n:
+                    find_flg = False
                     children = node.get_children()
-                    continue
+                    child_list = filter(lambda c: c.get_val() != n, children )
+                    if not child_list:
+                        rest_tel_num = telephone[i+1:]
+                        node.add_tree(rest_tel_num)
+                        print >> sys.stderr, rest_tel_num
+                        break
+                    else:
+                        node = child_list[0]
+                        continue
+                else:
+                    rest_tel_num = telephone[i:]
+                    node._parent.add_tree(rest_tel_num)
+                    print >> sys.stderr, rest_tel_num
+                    break;
         else:
             new_tree = TelNumberTree(telephone)
             # print >> sys.stderr, "telephone :" + str(telephone[0])
